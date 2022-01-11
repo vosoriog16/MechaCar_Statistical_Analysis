63dd23d7ad82bdf0018c6b1b7e87b6e80a73d3e9
vosoriog16 <88119288+vosoriog16@users.noreply.github.com>
2022-01-11 15:14:53 -0500

    upload new file

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,12 +1,13 @@
+#Deliverable 1
 library(dplyr)
 mecha_df <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data = mecha_df) #create linear regression
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data = mecha_df)) #create summary
-
+#Deliverable 2
 suspension_coil <-read.csv('Suspension_Coil.csv', check.names=F,stringsAsFactors = F) #create dataframe
 total_summary <- suspension_coil %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep') #create total summary table 
 lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep') #create lot summary table 
-
+#Deliverable 3
 t.test(suspension_coil$PSI,mu=1500) # sample versus population mean
 t.test(subset(suspension_coil$PSI,suspension_coil$Manufacturing_Lot== "Lot1"),mu=1500) #t-test for 1 
 t.test(subset(suspension_coil$PSI,suspension_coil$Manufacturing_Lot== "Lot2"),mu=1500) #t-test for 2 
